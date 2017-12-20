d2ffcb0d3d996847c2677c66346684dbab5933a0
eliotrhys <eliot@eliotshort.co.uk>
2017-12-20 11:20:25 +0000

    working with the first intent thing done

---
+++ app/src/main/java/com/example/eliotshort/rockpaperscissors/WelcomeActivity.java
@@ -0,0 +1,27 @@
+package com.example.eliotshort.rockpaperscissors;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    Button playButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+        playButton = findViewById(R.id.play_button);
+    }
+
+    public void onPlayButtonClick(View view){
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+    }
+
+
+}
